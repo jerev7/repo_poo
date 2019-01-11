ac5fa5cac94e0b933e518183ca1473586a9143e2
jerev7 <verdet.jeremie@gmail.com>
2019-01-11 14:29:28 +0100

    ajouté attribut et méthode à ma classe Agent

--- model.py
+++ model.py
@@ -1,9 +1,14 @@
 # -*- coding: utf8 -*-
 
 class Agent:
-	pass
+	
+	def say_hello(self, first_name):
+		return "Bien le bonjour " + first_name + " !"
 
-first_agent = Agent()
-second_agent = Agent()
+	def __init__(self, agreeableness):
+		self.agreeableness = agreeableness	
 
-print(first_agent, second_agent)
+agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": false, "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
+
+agent = Agent(0)
+print(agent.agreeableness)
