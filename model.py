02373e1d566224e81b211e747433f6d4a8da0b25
jerev7 <verdet.jeremie@gmail.com>
2019-01-17 19:37:49 +0100

    ajouté la fonction setattr pour pouvoir lire tous les éléments du dictionnaire

--- model.py
+++ model.py
@@ -6,11 +6,12 @@ class Agent:
 		return "Bien le bonjour " + first_name + " !"
 
 	def __init__(self, agent_attributes):
-		self.agreeableness = agent_attributes['agreeableness']	
+		for attr_name, attr_value in agent_attributes.items():
+			setattr(self, attr_name, attr_value)
 
-agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, 
+les_attributs = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, 
 "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, 
 "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": "false", "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}
 
-agent = Agent(agent_attributes)
-print(agent.agreeableness)
+agent = Agent(les_attributs)
+print(agent.openness)
