8d5c4513e791e1098fa45242e1b3edcbfd03cfa9
jerev7 <verdet.jeremie@gmail.com>
2019-01-17 21:43:27 +0100

    ajout de la classe Position et de l'instance position dans les attributs de la classe Agent

--- model.py
+++ model.py
@@ -6,13 +6,22 @@ class Agent:
 	def say_hello(self, first_name):
 		return "Bien le bonjour " + first_name + " !"
 
-	def __init__(self, **agent_attributes):
+	def __init__(self, position, **agent_attributes):
+		self.position = position
 		for attr_name, attr_value in agent_attributes.items():
 			setattr(self, attr_name, attr_value)
 
+class Position:
+	def __init__(self, longitude, latitude):
+		self.longitude = longitude
+		self.latitude = latitude
+
 def main():
 	for agent_attributes in json.load(open("agents-100k.json")):
-		agent = Agent(**agent_attributes)
-		print(agent.agreeableness)
+		longitude = agent_attributes.pop("longitude")
+		latitude = agent_attributes.pop("latitude")
+		position = Position(longitude, latitude)
+		agent = Agent(position, **agent_attributes)
+		print(agent.position.latitude)
 
 main()
